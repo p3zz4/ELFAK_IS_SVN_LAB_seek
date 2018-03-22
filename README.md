5460c3fe22d53a3d3b78ca5112af2588720a8167
p3zz4 <pezzasto@gmail.com>
2018-03-22 15:05:14 +0100

    1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ELFAK_IS_SVN_LAB_seek
-Pretraga studenata koji iz predmeta "Informacioni sistemi" imaju isti obrazac i sličan zadatak
+
+#Pretraga studenata koji iz predmeta "Informacioni sistemi" imaju isti obrazac i sličan zadatak.
+
+#Skriptu staviti u folder "studentski radovi 2018 - laboratorijske vezbe" i pokrenuti u terminalu.
+
+#Pokretanje skripte na windowsu se preporučuje pomoću "git-bash"-a i to DUPLIM KLIKOM na skriptu. Ukoliko se koristi desni klik pa "open with git-bash" podrazumevani direktorijum za pretragu će biti c:\windows\system32 i skripta će prikazivati neko đubre.
---
+++ Scripts/IS_SVN_grep_windows.sh
@@ -0,0 +1,21 @@
+#
+#       Skripta za pronalaženje kolega koje rade slični obrazac/zadatak. 
+#       Verzija za windows
+#       p3zz4
+#
+
+echo -n "Unesite broj indeksa: "
+read indeks
+obrazac=$( grep -rh "Obrazac: " ./"$indeks" )
+echo $obrazac
+echo "--------------------------------------"
+echo "Takav obrazac imaju:"
+grep -rl "$obrazac" "./"
+echo "--------------------------------------"
+echo -n "Unesi par reci teksta sa početka svog svog zadatka (npr: Informacioni sistem sportske kladionice): "
+read zadatak
+echo "Moguće je da slican zadatak imaju:"
+grep -rihl "$zadatak" "./"
+echo
+echo "Zadatak izvršen."
+read 
